334b4a93f2f5278009ab6e483bd821dda7a5f8cd
asifulmamun <asifulmamun@gmail.com>
2024-05-05 18:39:30 +0600

    group users and layout add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth', 'verified')->prefix('user')->group(function() {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
